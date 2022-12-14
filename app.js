8c7635224e992ed6f72f5189d861f908a370eee3
SatwikTellakula <119725034+SatwikTellakula@users.noreply.github.com>
2022-12-14 10:42:40 +0530

    git commit -m "solution"

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
-
+const datefns = require("date-fns");
 const app = express();
 app.use(express.json());
 let db = null;
@@ -61,6 +61,12 @@ const hasCategoryAndPriorityProperties = (requestQuery) => {
   return (
     requestQuery.category !== undefined && requestQuery.priority !== undefined
   );
+};
+const hasTodoProperty = (requestQuery) => {
+  return requestQuery.todo !== undefined;
+};
+const hasdueDateProperty = (requestQuery) => {
+  return requestQuery.dueDate !== undefined;
 };
 
 app.get("/todos/", async (request, response) => {
@@ -128,4 +134,97 @@ app.get("/todos/", async (request, response) => {
   );
 });
 
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const getTodoByIdQuery = `
+    select * from todo where id=${todoId};
+    `;
+  const todo = await db.get(getTodoByIdQuery);
+  response.send(convertToDoDbObjectToResponseObject(todo));
+});
+
+app.get("/agenda/", async (request, response) => {
+  const { search_q = "", priority, status, category, dueDate } = request.query;
+  const getTodosQuery = `
+     select * from todo where due_date='${dueDate}';
+     `;
+  const toDoArray = await db.all(getTodosQuery);
+  response.send(
+    toDoArray.map((eachTodo) => convertToDoDbObjectToResponseObject(eachTodo))
+  );
+});
+
+app.post("/todos/", async (request, response) => {
+  const { id, todo, priority, status, category, dueDate } = request.body;
+  const createNewTodQuery = `
+    INSERT INTO todo (id,todo,priority,status,category,due_date)
+    VALUES(${id},'${todo}','${priority}','${status}','${category}','${dueDate}');
+    `;
+  await db.run(createNewTodQuery);
+  response.send("Todo Successfully Added");
+});
+
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  let UpdatetodoQuery = "";
+  const {
+    search_q = "",
+    todo,
+    priority,
+    status,
+    category,
+    dueDate,
+  } = request.query;
+  switch (true) {
+    case hasStatusProperty(request.query):
+      UpdatetodoQuery = `
+            UPDATE todo SET status='${status}'
+            WHERE id=${todoId};
+            `;
+      await db.run(UpdatetodoQuery);
+      response.send("Status Updated");
+      break;
+    case hasPriorityProperty(request.query):
+      UpdatetodoQuey = `
+            UPDATE todo SET priority='${priority}';
+            WHERE id=${todoId};
+            `;
+      await db.run(UpdatetodoQuery);
+      response.send("Priority Updated");
+      break;
+    case hasTodoProperty(request.query):
+      UpdatetodoQuery = `
+             UPDATE todo SET todo='${todo}'
+            WHERE id=${todoId};
+            `;
+      await db.run(UpdatetodoQuery);
+      response.send("Todo Updated");
+      break;
+    case hasCategoryProperty(request.query):
+      UpdatetodoQuery = `
+            UPDATE todo SET category='${category}'
+            WHERE id=${todoId};
+            `;
+      await db.run(UpdatetodoQuery);
+      response.send("Category Updated");
+      break;
+    case hasdueDateProperty(request.query):
+      UpdatetodoQuery = `
+            UPDATE todo SET due_date='${dueDate}'
+            WHERE id=${todoId};
+            `;
+      await db.run(UpdatetodoQuery);
+      response.send("Due Date Updated");
+      break;
+  }
+});
+
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const deleteToDoQuery = `
+    DELETE FROM todo where id=${todoId};
+    `;
+  await db.run(deleteToDoQuery);
+  response.send("Todo Deleted");
+});
 module.exports = app;
